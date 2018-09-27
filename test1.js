9475da2b2cee36a653f061aaa70d45b3e89109b6
logickee <logickee@gmail.com>
2018-09-28 02:55:17 +0800

    init commit,

---
+++ test1.js
@@ -0,0 +1,7 @@
+//
+
+function foo(a) {
+	console.log( a ); // 2
+}
+
+foo( 2 );
---
+++ test2.js
@@ -0,0 +1,14 @@
+//
+
+'use strict';
+
+function foo(a) {
+	var b = a;
+	return a + b;
+}
+
+var c = foo( 2 );
+
+
+
+console.log(c);
